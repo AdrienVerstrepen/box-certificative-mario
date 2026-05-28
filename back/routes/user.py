2cb2e064402089a663e719228af5738888f9dbd9
obus325 <mathysbs@orange.fr>
2026-05-28 15:31:01 +0200

    separation

--- back/routes/user.py
+++ back/routes/user.py
@@ -1,26 +1,6 @@
-import psycopg2
-import os
-import flask
+from app import api, Connection
 
-app = flask.Flask(__name__)
-
-db_name = os.getenv('POSTGRES_DB')
-db_user = os.getenv('POSTGRES_USER')
-db_password = os.getenv('DB_PASSWORD')
-
-def Connection():
-    """
-    This function establishes a connection to the PostgreSQL database.
-    """
-    conn = psycopg2.connect(
-    dbname=db_name,
-    user=db_user,
-    password=db_password,
-    host='localhost',
-    )
-    return conn
-
-@app.route("/login", methods=['GET'])
+@api.route("/login", methods=['GET'])
 def login(email, password):
     """
     This function returns the information of a user if the provided credentials are correct.
@@ -45,7 +25,7 @@ def login(email, password):
     conn.close()
 
 
-@app.route("/register", methods=['GET'])
+@api.route("/register", methods=['GET'])
 def register(name, email, password):
     """
     This function registers a new user in the database.
