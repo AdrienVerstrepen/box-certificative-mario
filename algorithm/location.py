7a428ab5d9f34f337624ad01f98c7094cd9dbaf6
AtheneR <athene.rousseaurambach@gmail.com>
2026-05-28 13:50:40 +0200

    Added base structure algorithm : algorithm folder + tour class + location class + tests

--- algorithm/location.py
+++ algorithm/location.py
@@ -4,6 +4,9 @@ from haversine import Unit
 
 class Location:
     def __init__(self, city_id, name, latitude, longitude):
+        """
+        The location is represented by a city id, a name and coordinates, that can be accessed directly or the two of them together.
+        """
         self.id = city_id
         self.name = name
         self.latitude = latitude
@@ -11,7 +14,13 @@ class Location:
         self.coordonates = (latitude, longitude)
 
     def distance_locations(self, location_2):
+        """
+        The distance between two coordinates is calculated using the haversine library which calculates the distance bewteen two points on the planet using the surface of a sphere.
+        """
         return hs.haversine(self.coordonates, location_2.coordonates, unit=Unit.KILOMETERS)
 
     def __repr__(self):
+        """
+        This method shows the information of the location.
+        """
         return f"{self.name} (ID : {self.id})"
---
+++ algorithm/tour.py
@@ -0,0 +1,69 @@
+from algorithm.location import Location
+
+class Tour:
+    def __init__(self, locations = None):
+        """
+        A tour is represented by a list of locations and a matrix of the distances between each location.
+        The locations are ordered in the location list by their visit order.
+        """
+        if locations is not None :
+            self.locations = locations
+        else :
+            self.locations = []
+        self.distance_matrix = []
+        if self.locations:
+            self.generate_distance_matrix()
+
+    def add_location(self, location):
+        """
+        This method adds a location to the tour and updates the distance matrix.
+        """
+        self.locations.append(location)
+        self.generate_distance_matrix()
+
+    def generate_distance_matrix(self):
+        """
+        This method generates a graph represented by an adjacency matrix, where each cell (the value at matrix[i][j]) contains the distance between location i and location j.
+        """
+        n = len(self.locations)
+        distance_matrix = []
+        for i in range(n):
+            distance_matrix.append([0.0] * n)
+        self.distance_matrix = distance_matrix
+        for i in range(n):
+            for j in range(n):
+                if i != j:
+                    self.distance_matrix[i][j] = self.locations[i].distance_locations(self.locations[j])
+
+    def tour_score(self, itinerary_indices):
+        """
+        This method calculates the total distance of a given sequence of location indices.
+        The path returns to the starting location to complete the tour.
+        """
+        if not itinerary_indices or len(itinerary_indices) < 2:
+            return 0.0
+        total_distance = 0.0
+        for i in range(len(itinerary_indices) - 1):
+            index_start = itinerary_indices[i]
+            index_end = itinerary_indices[i+1]
+            total_distance += self.distance_matrix[index_start][index_end]
+        return total_distance
+    
+    def format_itinerary(self, itinerary_indices):
+        """
+        This method takes an ordered list of indices and pairs each Location with its visit order.
+        """
+        tour_itinerary = []
+        for order, index in enumerate(itinerary_indices):
+            location = self.locations[index]
+            tour_itinerary.append({
+                "visit_order": order + 1,
+                "location_object": location
+            })
+        return tour_itinerary
+
+    def __repr__(self):
+        """
+        This method shows the information of the tour.
+        """
+        return f"Tour with {len(self.locations)} locations."
