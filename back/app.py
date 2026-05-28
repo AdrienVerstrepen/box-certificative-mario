f0d00146b8ac1a37e9dc5e24a42237027b11837b
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-05-28 17:11:42 +0200

    Fixed back-end structure & fixed front-end API Calls

--- back/app.py
+++ back/app.py
@@ -1,23 +1,22 @@
 import psycopg2
 import os
 import flask
+from routes.user import userBlueprint
 
-api = flask.Flask(__name__)
+app = flask.Flask(__name__)
+
+app.register_blueprint(userBlueprint)
 
 db_name = os.getenv('POSTGRES_DB')
 db_user = os.getenv('POSTGRES_USER')
 db_password = os.getenv('DB_PASSWORD')
-db_host = os.getenv('DB_HOST', 'db')
+db_host = os.getenv('DB_HOST', 'localhost')
 
-def Connection():
-    """
-    This function establishes a connection to the PostgreSQL database.
-    """
-    conn = psycopg2.connect(
-        dbname=db_name,
-        user=db_user,
-        password=db_password,
-        host=db_host,
-        port='5432'
-    )
-    return conn
+@app.route("/")
+def index():
+    return "Homepage of GeeksForGeeks"
+
+import routes.user
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
