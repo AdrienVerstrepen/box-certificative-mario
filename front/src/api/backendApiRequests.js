1b0bdafa1de70ad2411666ddefdd9dfe2e924149
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-05-29 14:30:21 +0200

    Fixed tourName sending

--- front/src/api/backendApiRequests.js
+++ front/src/api/backendApiRequests.js
@@ -45,7 +45,7 @@ export const sendLoginRequest = async (mail, password) => {
 
 export const sendPlacesRequest = async (tourName, places) => {
     try {
-        const response = await backendClientConf.post('/api/places', tourName, places)
+        const response = await backendClientConf.post('/api/places', {tourName, places})
         return response.data
     } catch (error) {
 
