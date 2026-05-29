7cc86bbb14493f056ce0b7bfb5a5b2156a7532c0
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-05-29 13:43:17 +0200

    Deleted old directory

--- algorithm/path_finding_algorithms/base_path_finding_algorithm.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class BasePathFindingAlgorithm(ABC):
-    @abstractmethod
-    def find_shortest_itinerary(self, distance_matrix):
-        """
-        This method solves the problem of the shortest path by taking a distance matric of the locations and returning the list of the id of the locations, making an itinerary.
-        """
-        pass 
